c4b4a9738faeb3a059c6465509233755a1baf0a3
Bailey Knezevich <69914817+baileyknez@users.noreply.github.com>
2022-02-25 16:33:17 -0500

    got the player completely working
    how we just need to get the computer

--- speedJS.jsx
+++ speedJS.jsx
@@ -4,25 +4,58 @@ const game =GameMatch();
 console.log(game);
 let selectedId;
 let dropTargetId;
+let selectedValue;
+let dropTargetValue;
+let playerOutToggle=false;
 
-function Card(props){
-    var image;
-    if(props.name=="Computer"){
-    image = <img className="card" src="backOfCard.png" alt={props.card.value}/>
-    }else{
-    image= <img className="card" src={props.card.URL} alt={props.card.value}/>
-    }
+function PlayerCard(props){
+    var image= <img className="card" src={props.card.URL} alt={props.card.value}/>
+    
     return(
-    <div draggable={props.drag} className= {props.name} id={props.card.code} value={props.card.value} >
+    <div draggable={props.drag} className= {props.name} id={props.card.code} value={props.card.value} onDragStart={props.dragStart} onDragOver={props.dragOver}>
        {image}
     </div>
     
     );
 }
-function checkForMatch(selectedID, dropTargetId){
-    var value=selectedID;
-    var drop=dropTargetId;
-    console.log()
+function FieldCard(props){
+    var image= <img className="card" src={props.card.URL} alt={props.card.value}/>
+    return(
+    <div draggable={props.drag} className= {props.name} id={props.card.code} value={props.card.value} onDrop={props.dragDrop} onDragOver={props.dragOver}>
+       {image}
+    </div>
+    
+    );
+}
+function Card(props){
+    var image= <img className="card" src="backOfCard.png" alt={props.card.value}/>
+    return(
+    <div draggable={props.drag} className= {props.name} id={props.card.code} value={props.card.value}>
+       {image}
+    </div>
+    
+    );
+}
+function PlayerOut(props){
+    var image= <img className="card" src="backOfCard.png" alt="playerOutDeck"/>
+    
+    return(
+    <div draggable="true" className= "playerOut" onDragStart={props.dragPOStart} onDragOver={props.dragOver}>
+       {image}
+    </div>
+    
+    );
+}
+function Blank(props){
+    return(
+            <img className="card" src="backOfCard.png" alt={props.Name}/>
+    )
+}
+
+function checkForMatch(selectedValue, dropTargetValue){
+    var value=selectedValue;
+    var drop=dropTargetValue;
+
     if(value == 'JACK'){
         value=11;
     }else if(value== 'QUEEN'){
@@ -32,7 +65,7 @@ function checkForMatch(selectedID, dropTargetId){
     } else if(value=='ACE'){
         value="ACE";
     }else{
-         value=parseInt(selectedID);
+         value=parseInt(selectedValue);
     }
     if( drop== 'JACK'){
         drop=11;
@@ -43,14 +76,14 @@ function checkForMatch(selectedID, dropTargetId){
     } else if(drop=='ACE'){
         drop="ACE";
     }else{
-     drop=parseInt(dropTargetId);
+     drop=parseInt(dropTargetValue);
     }
     var higher = drop+1;
     var lower =drop-1;
-    if(value=="ACE" && drop==2 || drop==13){
+    if(value=="ACE" && (drop==2 || drop==13)){
         console.log("ace"+value +""+ drop)
         return true;
-    }else if(drop=="ACE" && value==2 || value==13){
+    }else if(drop=="ACE" && (value==2 || value==13)){
         console.log("ace"+value +""+ drop)
         return true;
     }else if(value == higher){
@@ -70,28 +103,34 @@ function checkForMatch(selectedID, dropTargetId){
 class Board extends React.Component{
     renderField(i){
         if(i<this.props.field.length){
+            var v =this.props.field[i];
         return(
-            <Card 
+            <FieldCard 
             name="Field"
             drag="false"
             card={this.props.field[i]}
+            dragOver={(ev) =>this.props.dragOver(ev)}
+            dragDrop={()=>this.props.dragDrop(v)}
             />
         );  
         }
     }
     renderPlayer(i){
-        if(i<this.props.playerHand.length){
+        if(this.props.playerHand[i]!=undefined){
+            var v =this.props.playerHand[i];
             return(
-                <Card 
+                <PlayerCard 
                 name="Player"
                 drag="true"
                 card={this.props.playerHand[i]}
+                dragStart= {() =>this.props.dragStart(v)}
+                dragOver={(ev)=>this.props.dragOver(ev)}
                 />
             );
             }   
     }
     renderComputer(i){
-        if(i<this.props.computerHand.length){
+        if(this.props.computerHand[i]!=undefined){
             return(
                 <Card 
                 name="Computer"
@@ -100,6 +139,28 @@ class Board extends React.Component{
                 />
             );  
             }
+    }
+    renderPlayerOut(){
+        if(this.props.playerOutLength != undefined){
+        return(
+            <PlayerOut
+                name=""
+                drag="true"
+                dragPOStart= {(ev) =>this.props.dragPOStart(ev)}
+                dragOver={(ev)=>this.props.dragOver(ev)}
+            />
+        );
+        }
+    }
+    renderComputerOut(){
+        if(this.props.computerOutLength != undefined){
+            
+            return(
+                <Blank
+                    name="ComputerOutDeck"
+                />
+            );  
+        }
     }
    
     render(){
@@ -113,11 +174,15 @@ class Board extends React.Component{
            {this.renderComputer(4)}
            </div>
            <div className="field">
+           <div className="computerOut">
+               {this.renderComputerOut()}
+           </div>
            {this.renderField(0)}
            {this.renderField(1)}
            <div className="containerField">
-           <div className="computerOut"></div>
-           <div className="playerOut"></div>
+           <div className="playerOut">
+               {this.renderPlayerOut()}
+           </div>
            </div>
            </div>
            <div className="hand">
@@ -141,45 +206,47 @@ class Game extends React.Component{
          computerHand:game.computerHand,
          computerDeck:game.computerDeck,
          playerOut:game.playerOut,
-         computerOut:game.computerOut,
+         computerOut:game.computerOut
         };
-    }
-    AddEventListeners() {
-        console.log("mount")
-       for(var i=0;i<this.state.playerHand.length;i++){
-        var x =document.getElementById(this.state.playerHand[i].code)
-        x.addEventListener('dragstart', this.dragStart);
-        x.addEventListener('dragover', this.dragOver);
-       }
-       for(var i=0;i<this.state.field.length;i++){
-        var x =document.getElementById(this.state.field[i].code)
-        x.addEventListener('drop', this.dragDrop);
-        x.addEventListener('dragover', this.dragOver);
-       }
-    }
-    componentDidMount(){
-        this.AddEventListeners();
+        
     }
     //drag and drop functionalit
-     dragStart(){
-        selectedId=document.getElementById(this.id).getAttribute('value');
-        console.log(selectedId);
+     dragStart(card){
+        selectedId=card.code;
+        selectedValue =card.value;
+        console.log(selectedValue);
     }
-    dragOver(ev) {
+    dragOver(ev){
         ev.preventDefault();
     }
-    
-    dragDrop(){
-        dropTargetId=document.getElementById(this.id).getAttribute('value');
-        console.log(dropTargetId);
-       if(checkForMatch(selectedId,dropTargetId)){
+    dragDrop(card){
+        dropTargetId=card.code;
+        dropTargetValue=card.value;
+        console.log(dropTargetValue);
+       if(playerOutToggle){
+        this.playerOutMatch()
+       }else{
+       if(checkForMatch(selectedValue,dropTargetValue)){
         console.log("match");
-        self.playerHandMatch(selectedId,dropTargetId);
+        this.playerHandMatch();
        }else{
            console.log("not a match")
        }
     }
-    
+    }
+    dragPOStart(ev){
+        console.log("player out")
+        if(this.checkForValidMove(this.state.playerHand,this.state.field)){
+        ev.preventDefault();
+        playerOutToggle=false;
+        }else{
+        playerOutToggle=true;
+        var length=this.state.playerOut.length;
+        selectedId=this.state.playerOut[length-1];
+        console.log(selectedId);
+        }
+    }
+   
     calculateWinner(){
         if(this.state.computerHand.length ==0 && this.state.computerDeck.length == 0){
         return ("Computer Wins")
@@ -188,37 +255,74 @@ class Game extends React.Component{
         return("You win!");
         }
     }
-   playerHandMatch =(selectedID,dropTargetId) =>{
     
-    const newField=this.state.field;
-    const newPlayerHand=this.state.playerHand;
-    const newDeck=this.state.playerDeck;
+   playerHandMatch(){
+    var newField=this.state.field;
+    var newPlayerHand=this.state.playerHand;
+    var newDeck=this.state.playerDeck.slice();
     var transfer;
     for(var i=0; i<newPlayerHand.length;i++){
-        if(newPlayerHand[i].value== selectedID ){
-            transfer= newPlayerHand[i].pop();
+        if(newPlayerHand[i].code== selectedId ){
+            transfer= newPlayerHand.splice(i,1);
         }
     }
-       for(var i=0; i<newfield.length;i++){
-           if(newField[i].Card.value ==dropTargetId){
-              newField[i]=transfer;
+       for(var i=0; i<newField.length;i++){
+           if(newField[i].code ==dropTargetId){
+              newField[i]=transfer[0];
            }
        }
        newPlayerHand.push(newDeck.pop());
-    this.setState({
+       this.setState({
         playerHand:newPlayerHand.slice(),
         playerDeck:newDeck.slice(),
         field:newField.slice()
     })
    }
+   playerOutMatch(){
+    var newField=this.state.field;
+    var newOut=this.state.playerOut.slice();
+    var transfer=newOut.pop();
+    console.log(selectedId)
+    console.log(dropTargetId)
+       for(var i=0; i<newField.length;i++){
+           if(newField[i].code ==dropTargetId){
+              newField[i]=transfer;
+              console.log(newField);
+           }
+       }
+       this.setState({
+        playerOut:newOut.slice(),
+        field:newField.slice()
+    })
+
+   }
+   checkForValidMove(hand,field){
+     var bool=false;
+    for(var i=0; i< hand.length;i++){
+        for (var x=0;x<field.length;x++){
+            if(checkForMatch(hand[i].value, field[x].value)==true){
+                console.log("you still have a move");
+                bool=true;
+            }
+        }
+    }
+    return bool;
+   }
+ 
     //render Board
     render(){
         return(
         <div className="board">
           <Board 
               playerHand={this.state.playerHand}
+              playerOutLength={this.state.playerOut.length}
               field={this.state.field}
               computerHand={this.state.computerHand}
+              computerOutLength={this.state.computerOut.length}
+              dragStart={card => this.dragStart(card)}
+              dragOver={ev =>this.dragOver(ev)}
+              dragDrop={card => this.dragDrop(card)}
+              dragPOStart={ev => this.dragPOStart(ev)}
           />
         </div>
         );
@@ -230,4 +334,3 @@ class Game extends React.Component{
         <Game />,
         document.getElementById('root'),
     ); 
-
